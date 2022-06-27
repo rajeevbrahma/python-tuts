d2b069807f23f6099809427d2b0a973395df4b70
prowess-dev <prowess-dev@prowesss-MacBook-Air.local>
2022-06-27 19:34:46 +0530

    intro and variable progs added

---
+++ prog_intro.py
@@ -0,0 +1,30 @@
+
+
+'''
+    single 
+    quotes 
+    bulk 
+    comment
+
+'''
+
+
+"""
+    double 
+    qoutes 
+    bulk 
+    comment
+
+"""
+
+# single comment
+
+print ('\nhelloworld',5)
+print ('\thelloworld',5)
+
+# print ('a'+5)
+
+
+
+
+
---
+++ prog_variables.py
@@ -0,0 +1,31 @@
+
+
+'''
+    # data types
+
+    Integer
+    Float
+    String
+    
+'''
+
+'''
+    Example variable declaration
+
+         5, 5.02, 'swathi' / "swathi" / """swathi"""
+'''
+
+
+
+a = 'rajeev'
+
+print (a)
+
+# int a = 10
+# a[][][][] <- 10
+# int b = 10
+# b[][][][]10 
+
+# python storage process
+#     [10]        <- a <- b
+#(4306772520)
