71f01e6cc550895b0c292fe7cff987e8cb96cd8b
prowess-dev <rajeevbrahma@hotmail.com>
2022-07-12 21:36:40 +0530

    added function in variable prog, added gitignore file

--- prog_variables.py
+++ prog_variables.py
@@ -19,7 +19,7 @@
 
 a = 'rajeev'
 
-print (a)
+# print (a)
 
 # int a = 10
 # a[][][][] <- 10
@@ -29,3 +29,15 @@ print (a)
 # python storage process
 #     [10]        <- a <- b
 #(4306772520)
+
+
+def addition(a,b):
+    if (type(a) == int and type(b) == int):
+        return (a+b)
+    return None
+
+sut = addition(4,'abd')   # test case 1 
+print (sut)
+# assert sut is None
+sut = addition(4,5)
+assert type(sut) is int   # test case 2
