2cee1d16c649060ac6643e25e9fe3b51438fb0bb
rajeevbrahma <rajeevbrahma@hotmail.com>
2022-08-06 10:27:30 +0530

    day 5 plan and list data types prog

---
+++ data_types_list.py
@@ -0,0 +1,35 @@
+"""
+
+Lists are ordered.
+Lists can contain any arbitrary objects.
+List elements can be accessed by index.
+Lists can be nested to arbitrary depth.
+Lists are mutable.
+Lists are dynamic.
+
+
+"""
+
+
+list_example = [1,2,3,"raj",4.5,True,[1,2,3]]
+
+
+
+bio_data = [
+    "John",
+    "",
+    "Kurup",
+    25,
+    169,
+    80.78,
+    True,
+    None,
+    "5'10",
+    ' This is some descriptions \
+about "Mr John" he is very \
+asdfasdfads \
+jsa;ldkjfasldjfa \
+'
+]
+
+print (bio_data)
