0e0fbddd4b651e866921102c0d6f123e636d15c6
rajeevbrahma <rajeevbrahma@hotmail.com>
2022-08-09 13:27:05 +0530

    operators in python

---
+++ programs/operators/operators_in_python.py
@@ -0,0 +1,63 @@
+"""
+
+        Arithmetic operators
+                                +, -, *, **, /, %, //
+        Assignment operators
+                                =, +=, -=, *=, /=, %=, //=, **=, &=, |=, ^=, >>=, <<=
+        Comparison operators
+                                ==, != , > , < , >=, <=
+        Logical operators
+                                and, or, not        
+        Identity operators
+                                is, is not
+        Membership operators
+                                in, not in
+
+
+"""
+
+# Arithmetic operators
+
+from re import A
+
+
+print (1+2)
+print (2-1)
+print (3*4)
+print (3**4)
+print (3/4)
+print (3%4)
+print (3//4)
+
+# Assignment operators
+
+assignee = 10
+print (assignee)
+
+assignee += 10   # => assignee = assignee + 10
+print (assignee)
+assignee -= 10   # => assignee = assignee - 10
+print (assignee)
+assignee *= 10   # => assignee = assignee * 10
+print (assignee)
+assignee **= 10   # => assignee = assignee ** 10
+print (assignee)
+assignee /= 10   # => assignee = assignee / 10
+print (assignee)
+assignee %= 10   # => assignee = assignee % 10
+print (assignee)
+assignee //= 10   # => assignee = assignee // 10
+print (assignee)
+# assignee = 0.0
+# assignee &= 10.0   # => assignee = assignee & 10
+# print (assignee)
+# assignee |= 10   # => assignee = assignee | 10
+# print (assignee)
+# assignee ^= 10   # => assignee = assignee ^ 10
+# print (assignee)
+# assignee <<= 10   # => assignee = assignee << 10
+# print (assignee)
+# assignee >>= 10   # => assignee = assignee >> 10
+# print (assignee)
+
+
