eef46b8190dc69266294203f210b2d1efa24f6c0
rajeevbrahma <rajeevbrahma@hotmail.com>
2022-08-09 12:47:52 +0530

    how to define variable names

---
+++ programs/variables/how_to_define_variables.py
@@ -0,0 +1,32 @@
+
+"""
+    Valid way of defining variables
+
+"""
+firstname = "Kareena"               # writing two words as single name as variable name
+first_name = "Kareena"              # using underscore to connect to words as variable name
+_first_name = "Kareena"             # we can underscore before variable name and is suggested
+                                    # for private variables only    
+firstName = "Kareena"               # this is camel case way of defining variables and suggested 
+                                    # function names of class names
+FIRSTNAME = "Kareena"               # suggested for constant names
+firstname1 = "Kareena"              # can use numbers at the end of a word as a variable name
+
+name_1 = name_2 = "Kareena"             # same value to multiple variables
+
+first_name,last_name = "Kareena","K"    # multiple values to multiple variables
+
+
+"""
+    Invalid way of defining variables
+
+"""
+
+
+1firstname = "Kar"                 # cannot use number at the begining of word for a variable name
+first-name = "Kareena"             # cannot use "-" to connect two words as this will be considered
+                                   # value being associated to an arithmetic expression which is not allowed  
+first name = "Kareena"             # space cannot be used for variable names
+
+
+
