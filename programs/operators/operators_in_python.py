8879dc887ab52a39879f1d0a5651614701576b3b
rajeevbrahma <rajeevbrahma@hotmail.com>
2022-08-19 06:11:50 +0530

    operators program

--- programs/operators/operators_in_python.py
+++ programs/operators/operators_in_python.py
@@ -1,24 +1,32 @@
 """
 
         Arithmetic operators
-                                +, -, *, **, /, %, //
+                                +, -, *, 
+                                **,   - 2**3 => 2 to the power of 3  - Exponentiation
+                                /,    - 2/3  =>  Division which results quotient - division
+                                %,    - 3%2 => Division which results the remainder - Modulus
+                                 //   - 3//2 => Divion which results the floored quotient - floor division
+
         Assignment operators
                                 =, +=, -=, *=, /=, %=, //=, **=, &=, |=, ^=, >>=, <<=
         Comparison operators
                                 ==, != , > , < , >=, <=
+                                int,float,str,list
+                                
         Logical operators
                                 and, or, not        
         Identity operators
                                 is, is not
         Membership operators
                                 in, not in
+        
+        Bitwise operators 
+                                & | ^ 
 
 
 """
 
 # Arithmetic operators
-
-from re import A
 
 
 print (1+2)
@@ -80,14 +88,22 @@ print (not 1)
         And Truth Table
         1 and 1 1
         0 and 1 0     
-        1 and 0 
-        0 and 0
+        1 and 0 0
+        0 and 0 0
+
+        and returns the first operand if is false otherwise it returns the second
+        operand
+
+        
 
         Or Truth Table
-        1 or 1
-        0 or 1
-        1 or 0
-        0 or 0
+        1 or 1 1
+        0 or 1 1
+        1 or 0 1
+        0 or 0 0
+
+        or returns the first operand if it is true otherwise it returns second 
+        operand
 
 Exercise - try these combinations with both "and" and "or"
 
@@ -116,3 +132,33 @@ name_1 = "pyt"
 name_2 = "python"
 print (name_1 in name_2)
 print (name_1 not in name_2)
+
+
+"""
+
+        a = "ok"
+
+        a = a[::-1]
+        a
+        "ko"
+
+        Note : String doesnt have a method which does reverse the string
+        we can use the negative step 1 to reverse
+
+
+
+
+firstname == "joy"
+then
+        lastname == "k"
+
+firstname = ""
+lastname = "k"
+
+if (firstname == "joy" or lastname == "k")
+        print ("yes")
+
+
+
+
+"""
