39f3768f65af9a7c054a78892538091a5c9bd983
rajeevbrahma <rajeevbrahma@hotmail.com>
2022-08-30 19:57:32 +0530

    loops exercise solution 2

--- programs/loops/loops_exercise.py
+++ programs/loops/loops_exercise.py
@@ -1,4 +1,6 @@
 
+# solution 1
+
 even_numbers = []
 
 for outer_range in range(0,4):
@@ -11,11 +13,11 @@ for outer_range in range(0,4):
     if (outer_range < 1):
         start_index = 1
     else:
-        start_index = outer_range*100 - 1
+        start_index = outer_range*100 + 1
     
     for inner_range in range(start_index,100+start_index):
         
-        if (inner_range%2 != 0 or inner_range == 0):
+        if (inner_range%2 != 0):
             continue
         
         even_numbers_dict.update({dict_key_name:inner_range})
@@ -25,3 +27,54 @@ for outer_range in range(0,4):
     even_numbers[outer_range] = even_numbers_dict
 
 print (even_numbers)
+
+
+
+"""
+    [
+        {1:2,2:4....},
+        {1:102,2:204....},
+        {}
+
+    ]
+
+"""
+
+# solution 2 
+result_list = []
+number_of_dictionaries_in_list = 4 
+
+
+number_dict = {}    
+
+for each_dict_count in range(number_of_dictionaries_in_list):
+
+    for_loop_start_index = each_dict_count * 100 + 1   # 201
+
+    for_loop_stop_index = 100 + for_loop_start_index  # 301
+
+
+    dict_key_name = 1
+
+    for each_number in range(for_loop_start_index,for_loop_stop_index):
+                                        
+        if (each_number%2 == 0):            
+            number_dict.update({dict_key_name:each_number}) 
+        
+            dict_key_name+=1
+
+    result_list.append(number_dict)
+
+
+print (result_list)                 # {1:4}
+
+
+
+
+
+
+
+
+
+
+
--- programs/loops/loops_in_python.py
+++ programs/loops/loops_in_python.py
@@ -61,5 +61,127 @@ print (temp_string)
 # for character_index in range(2,len(example_string)-2,2):
 #     print (example_string[character_index])
 
+# a = {"a":"1","b":"2"}
+
+a = [1,2,3,4,5]
+            
+for i in a:
+    pass
+    # print (i)
+
+
+# range(start_index,stop_index+1,step)
+for index_number in range(0,len(a)): 
+    # print (a[index_number])
+    pass
+
+dict_variable = {"a":1,"b":2,"c":3,"d":4}
+
+# for key_name in dict_variable:
+#     print (dict_variable.keys())
+#     print (dict_variable[key_name])
+#     print (dict_variable.get(key_name))
+
+for key_name,value_name in dict_variable.items(): 
+    pass
+    # print (key_name,"-----",value_name)
+
+
+
+for dict_item in dict_variable.items():
+    # print (dict_item)
+    key_name,value_name = dict_item
+            # or 
+    # key_name = dict_item[0]
+    # value_name = dict_item[1]
+    # print (key_name,"-------",value_name)
+
+# for _ in range(5):
+#     print (_)
+#     print (type(_))
+# print(_)
+
+
+a = [1,2,3,4,5,5,6]
+
+
+"""
+
+while(<Tue>):   expx = a in "Oython" | a == b | a is b
+    print ("welcome")
+
+a = True
+
+while(a):
+    print ("welcom")    
+    a = False
+
+"""
+
+i = 0
+a = "Python"
+
+# 8<10 and None == "Python"
+while(i<10 and a == "Python"):
+    
+    print ("iteration - ",i,type(i<10),i<10,a)
+    # i = 7
+    if (i > 5):
+        a = None
+    print ("after condition check",i,type(i<10),i<10,a)
+    i+=1 # 7
+    print ("after i increment",i,type(i<10),i<10,a,"\n")
+
+print ("OUT OF LOOP")
+print (i,type(i<10),i<10,a)
+
+
+for i in range(10):
+    if (i == 5):
+        continue
+
+    print (i)
+
+# ------------ SRIHARSHA SUGGESTION CODE BLOCK -----------
+dict1 = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
+dict2 = {'Thirty': 50, 'Fourty': 40, 'Fifty': 50}
+dict3 = {**dict2,**dict1}
+print ("sriharsha",dict3)
+
+dict3 = {}
+dict3.update(dict1)
+dict3.update(dict2)
+
+print (dict3)
+
+print (id(dict1))
+print (id({**dict1}))
+# ----------------------
+
+# give me even numbers using continue while looping through 0 to 100
+
+even_numbers = []
+even_numbers_dict = {}
+index_variable = 1
+
+for i in range(1,100):
+    if (i%2 != 0):
+        continue
+    print (i,index_variable)
+    even_numbers_dict.update({index_variable:i})
+    index_variable +=1
+    
+    # even_numbers.append(i)
+
+# print (even_numbers)
+
+print (even_numbers_dict)
+
+
+# {1:2,2:4,3:6 ........ 49:98}
+
+# [{1:2,2:4,3:6 ........ 49:98},{1:102,2:104,3:106 ........ 49:198},{1:202,2:204,3:206 ........ 49:198}]
+
+
 
 
