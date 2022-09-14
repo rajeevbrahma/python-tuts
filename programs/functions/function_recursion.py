beac0b4e8510bbfe85c5bc7f3604747f4cd1ba99
rajeevbrahma <rajeevbrahma@hotmail.com>
2022-09-14 05:56:22 +0530

    functions

--- programs/functions/function_recursion.py
+++ programs/functions/function_recursion.py
@@ -1,58 +1,58 @@
 
-"""
+# """
 
-    10 
+#     10 
 
-    WRITE A FUNCTION WHICH TAKES AN INTEGER AS AN ARGUMENT 
-    AND DECREASE ONE STEP AT A TIME AND PRINT THE RESULT
+#     WRITE A FUNCTION WHICH TAKES AN INTEGER AS AN ARGUMENT 
+#     AND DECREASE ONE STEP AT A TIME AND PRINT THE RESULT
 
-    10
-    9
-    8
-    7
-    6
-    5
-    4
-    3
-    2
-    1
+#     10
+#     9
+#     8
+#     7
+#     6
+#     5
+#     4
+#     3
+#     2
+#     1
 
 
 
-"""
+# """
 
-def negative_step_func(a):
+# def negative_step_func(a):
 
-    for i in range(a):
-        print (a)
-        a-=1
+#     for i in range(a):
+#         print (a)
+#         a-=1
         
 
 
 
-def negative_step_func(a):
-    for i in range(a,0,-1):
-        print (i)
+# def negative_step_func(a):
+#     for i in range(a,0,-1):
+#         print (i)
  
-# negative_step_func(5)
+# # negative_step_func(5)
 
 
 
 
-def negative_step_recurv_func(a):
-    a-=1
-    print (a)
-    if (a>0):
-        negative_step_recurv_func(a) # recursive function call based on condition
+# def negative_step_recurv_func(a):
+#     a-=1
+#     print (a)
+#     if (a>0):
+#         negative_step_recurv_func(a) # recursive function call based on condition
 
-# negative_step_recurv_func(10) # normal function call
+# # negative_step_recurv_func(10) # normal function call
 
-# factorial of a number using recursion
+# # factorial of a number using recursion
 
 
 result = 1
 
-# 7*6*5*4*2*1
+# # 7*6*5*4*2*1
 
 
 def factorial_of_a_number_using_recursion(result,a):
@@ -64,9 +64,8 @@ def factorial_of_a_number_using_recursion(result,a):
         factorial_of_a_number_using_recursion(result,a) #recursive function call    
     
 
-    
 
-factorial_of_a_number_using_recursion(result,7) # function call 
+# factorial_of_a_number_using_recursion(result,7) # function call 
 
 
 
@@ -76,3 +75,101 @@ factorial_of_a_number_using_recursion(result,7) # function call
 #     return a*recur_func(a-1)
 
 # print (recur_func(7))
+
+
+
+
+# Write a program to add a number to itself till it reaches a maximum value
+
+maximum_value = 60
+start_number = 1
+
+def add_number(number):   # 1
+
+    number+=number      # business logic -> 2
+    
+    if (number < maximum_value):   # True
+        print (number)             # 40
+        add_number(number)         # recursion function call with 40
+        
+
+print ("hello")
+        
+# add_number(start_number)            # normal function call
+
+
+# write a program to add squares of a number to list till it reaches maximum value
+
+#[1,4,9,16,25.....100]
+
+start_number = 1
+maximum_value = 10
+list_variable = []
+
+def squaring_numbers(number):    # 1
+    
+    square_of_a_number = pow(number,2)  # squaring a number 1
+    
+    list_variable.append(square_of_a_number)  # adding the result to the list [1]
+    
+    if (number < maximum_value):    # checking for the recusion condition True
+        print (number)          # 1
+        number = number + 1
+        squaring_numbers(number) # recursion function call
+        
+squaring_numbers(start_number)  # function call
+
+print (list_variable)
+
+
+
+
+"""
+    [room 1 [room 2 [room 3] ] ]
+
+
+    enter a room 1
+            switch off tv 1
+            
+            entering room 2
+                switch off tv 2
+                
+                entering room 3
+                    swtich off tv 3
+                    switch off ac 3
+                
+                switch off ac 2 
+            
+            switch off ac 1
+
+
+
+
+
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+# def add_number(number):
+
+#     if (number > maximum_value):
+#         return number
+    
+#     return add_number(number+number)
+    
+
+# print (add_number(start_number))
+
+
+
+
--- programs/functions/function_scope_prog.py
+++ programs/functions/function_scope_prog.py
@@ -1,27 +1,52 @@
 
-def function_with_variable_within_it():
-    a = 500
-    print (a)
+# def function_with_variable_within_it():
+#     a = 500
+#     print (a)
 
-function_with_variable_within_it()
-print (a)
+# function_with_variable_within_it()
+# print (a)
 
 
 
-b = 300
-def function_with_variable_within_it():
-    b = 500
+# b = 300
+# def function_with_variable_within_it():
+#     b = 500
+#     print (b)
+
+# function_with_variable_within_it()
+# print (b)
+
+
+# def function_with_global_variable():
+#     global c
+#     c = 100
+#     print (c)
+
+# function_with_global_variable()
+# print (c)
+
+
+# globla space
+
+a = 100 # global variable
+
+
+def func():
+    # local space
+    # a = 500 # local variable
+    print ("Inside func",a)
     print (b)
 
-function_with_variable_within_it()
+def func_with_global_variable():
+    # global b  # We want this locally created variable to be treated as a global variable
+    global b
+    b = 400
+
+
+
+func_with_global_variable()   
+func()
+
+print (a)
 print (b)
-
-
-def function_with_global_variable():
-    global c
-    c = 100
-    print (c)
-
-function_with_global_variable()
-print (c)
 
